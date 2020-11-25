e547485293999e5d08983cd0e563578e71fb9a70
syousyuukoriki <5364tarou@gmail.com>
2020-11-25 19:22:08 +0900

    投稿したものをトップページに表示

--- app/controllers/diarys_controller.rb
+++ app/controllers/diarys_controller.rb
@@ -1,8 +1,22 @@
 class DiarysController < ApplicationController
   def index
+    @diarys = Diary.all
   end
 
   def new
     @diary = Diary.new
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to root_path
+    end
+  end
+
+private
+  
+  def diary_params
+    params.require(:diary).permit(:title, :day, :rate, :event, :image)
   end
 end
